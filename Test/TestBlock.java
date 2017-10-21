d69b0a6781a6185e60b525ce3a66fb746f1530a4
Jimmy Ho <pointyhe0@gmail.com>
2017-10-20 19:57:05 -0700

    Test

---
+++ Test/TestBlock.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+/**
+ * This class does something
+ */
+public class TestBlock {
+    public static void main(String[] a) {
+        System.out.println(Arrays.toString(a));
+    }
+}
